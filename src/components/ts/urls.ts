3bb03928af83f12e4f3a0f550aad6522da285883
Gustavo <truequemania.cl@gmail.com>
2024-12-30 21:17:41 -0400

    Cambios en las rutas

--- src/components/ts/urls.ts
+++ src/components/ts/urls.ts
@@ -1,2 +1,2 @@
-// export const api = "https://intercambio-backend-ashy.vercel.app";
-export const api = "http://localhost:4001"
+export const api = "https://intercambio-backend-ashy.vercel.app";
+// export const api = "http://localhost:4001"
